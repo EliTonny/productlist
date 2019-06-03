a21d0576744e1af11f0f496624eb931748f8f685
Eli <EliTonny@users.noreply.github.com>
2019-06-03 09:53:42 -0300

    Update productRouter.js

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -5,9 +5,9 @@ const productsController = require('../controllers/productsController.js');
 const router = express.Router();
 
 // GET /feed/posts
-router.get('/products', productsController.getProducts);
-
 router.get('/products/searchbytitle/:title', productsController.getProductsByTitle);
+router.get('/products', productsController.getProducts);
+router.get('/', productsController.getProducts);
 
 // POST /feed/post
 //router.post('/post', feedController.createPost);
